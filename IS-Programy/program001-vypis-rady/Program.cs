6aeab740d8713bab43a1ec38ef14d4235cc67d6f
Tomáš Havel <tomas.havel@tul.cz>
2025-10-02 14:05:31 +0200

    #3 Výpis řady - vstup první hodnoty

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,40 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********** Výpis číselné řady **************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*************** Tomáš Havel *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("**************** 2.10.2025 ******************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine();
+
+    //Vstup hodnoty do programu, ale špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+
+    //Console.WriteLine();
+    //Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    //again = Console.ReadLine();
+
+
+
+
+    //Vstup hodnoty do programu, řešený lépe
+    Console.Write("Zadejte první číslo řady: ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste cele cislo. Zadejte prvni cislo rady znovu: ");
+    } 
+
+
+
+
+}
