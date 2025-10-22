00101c54d7ac6c2607d676672134ef01df37f666
Tomáš Havel <tomas.havel@tul.cz>
2025-10-22 16:38:17 +0200

    #5 DU - Součet cifer vlastní řešení

---
+++ IS-Programy/program002b-soucet-cifer/Program.cs
@@ -0,0 +1,38 @@
+﻿string again = "a";
+while (again == "a")
+{
+
+
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("************** Součet cifer *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*************** Tomáš Havel *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("**************** 16.10.2025 *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte číslo: ");
+        string vstup = Console.ReadLine();
+
+        int soucet = 0;
+
+        foreach (char znak in vstup)
+        {
+            if (char.IsDigit(znak))
+            {
+                soucet += int.Parse(znak.ToString());
+            }
+        }
+
+        Console.WriteLine("Součet cifer: " + soucet);
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
