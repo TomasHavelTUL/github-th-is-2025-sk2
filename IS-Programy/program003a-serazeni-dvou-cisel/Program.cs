560637009a207a3f8ce6309c28703a7cf6568e7c
Tomáš Havel <tomas.havel@tul.cz>
2025-10-23 13:11:23 +0200

    #6 Prohozeni dvou promennych - program

--- IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
+++ IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
@@ -1,1 +1,54 @@
-﻿
+﻿using System.Runtime.InteropServices.Marshalling;
+
+string again = "a";
+while (again == "a")
+{
+
+
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("************ Program podle VD ***************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*************** Tomáš Havel *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("**************** 23.10.2025 *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte celé číslo A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste cele cislo. Zadejte znovu celé číslo A: ");
+    }
+
+    Console.Write("Zadejte celé číslo B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste cele cislo. Zadejte znovu celé číslo B: ");
+    }
+
+    Console.WriteLine();
+
+    int pom;
+    //Chceme seradit cisla vzestupne
+    if (a > b)
+    {
+        pom = a;
+        a = b;
+        b = pom;
+        Console.WriteLine("Došlo k prohození proměnných");
+    }
+
+    Console.WriteLine("====================================");
+    Console.WriteLine($"Sežazená čísla: {a}, {b}");    
+    Console.WriteLine("====================================");
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
