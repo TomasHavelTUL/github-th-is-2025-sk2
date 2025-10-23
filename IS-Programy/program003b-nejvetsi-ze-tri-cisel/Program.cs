a08b2f701721c2756f7c92db950ee63f71e2369b
Tomáš Havel <tomas.havel@tul.cz>
2025-10-23 13:43:18 +0200

    #6 Nejvetsi ze tri cisel - program

--- IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
+++ IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
@@ -1,1 +1,83 @@
-﻿
+﻿using System.Runtime.InteropServices.Marshalling;
+
+string again = "a";
+while (again == "a")
+{
+
+
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********** Nejvetsi ze 3 cisel *************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*************** Tomáš Havel *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("**************** 23.10.2025 *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte celé číslo A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste cele cislo. Zadejte znovu celé číslo A: ");
+    }
+
+
+    Console.Write("Zadejte celé číslo B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste cele cislo. Zadejte znovu celé číslo B: ");
+    }
+
+
+    Console.Write("Zadejte celé číslo C: ");
+    int c;
+    while (!int.TryParse(Console.ReadLine(), out c))
+    {
+        Console.Write("Nezadali jste cele cislo. Zadejte znovu celé číslo C: ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("=======================================");
+
+
+    if (a > b)
+    {
+        if (a > c)
+        {
+            Console.WriteLine($"Nejvetsi cislo je A ={a}");
+        }
+        else
+        {
+            Console.WriteLine($"Nejvetsi cislo je C = {c}");
+        }
+
+    }
+    else
+    {
+        if (b > c)
+        {
+            Console.WriteLine($"Nejvetsi cislo je B = {b}");
+        }
+        else
+        {
+            Console.WriteLine($"Nejvetsi cislo je C = {c}");
+        }
+
+    }
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
