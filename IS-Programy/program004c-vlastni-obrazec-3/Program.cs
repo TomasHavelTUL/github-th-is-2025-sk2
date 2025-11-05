d2eef791e5b3dd6190c4bd7da731f25b3a71e369
Tomáš Havel <tomas.havel@tul.cz>
2025-11-05 19:40:11 +0100

    #7 vykresleni vlastniho obrazce 3 - program

---
+++ IS-Programy/program004c-vlastni-obrazec-3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+string again = "a";
+while (again == "a")
+{
+
+
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("******* Vykresleni vlastniho obrazce ********");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*************** Tomáš Havel *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("**************** 5.11.2025 *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine();
+
+Console.Write("Zadejte pocet radku: ");
+int radky;
+while (!int.TryParse(Console.ReadLine(), out radky) || radky < 1)
+{
+    Console.Write("Nezadali jste cele cislo. Zadejte pocet radku znovu: ");
+}
+
+Console.Write("Zadejte pocet hvezdicek v radku: ");
+int sloupce;
+while (!int.TryParse(Console.ReadLine(), out sloupce) || sloupce < 1)
+{
+    Console.Write("Nezadali jste cele cislo. Zadejte pocet hvezdicek znovu: ");
+}
+
+for (int i = 0; i < radky; i++)
+{
+    if (i % 2 == 0)
+        Console.Write(" ");
+
+    for (int j = 0; j < sloupce; j++)
+    {
+        Console.Write("* ");
+        System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(80));
+    }
+
+    Console.WriteLine();
+}
+
+
+
+
+        Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
