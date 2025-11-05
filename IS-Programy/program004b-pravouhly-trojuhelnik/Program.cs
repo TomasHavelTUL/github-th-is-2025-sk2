ae91e149d7d380b4a635a326ad76966d32057e16
Tomáš Havel <tomas.havel@tul.cz>
2025-11-05 18:56:41 +0100

    #7 Vykresleni pravouhleho trojuhelniku - program

---
+++ IS-Programy/program004b-pravouhly-trojuhelnik/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+string again = "a";
+while (again == "a")
+{
+
+
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("**** Vykresleni pravouhleho trojuhelniku ****");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*************** Tomáš Havel *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("**************** 5.11.2025 *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte velikost pravouhleho trojuhelniku: ");
+    int size;
+    while (!int.TryParse(Console.ReadLine(), out size))
+    {
+        Console.Write("Nezadali jste cele cislo. Zadejte velikost znovu: ");
+    }
+
+    for (int i = 1; i <= size; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(150));
+        }
+        Console.WriteLine();
+    }
+}
