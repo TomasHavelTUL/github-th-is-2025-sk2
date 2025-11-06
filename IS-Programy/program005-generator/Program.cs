8952577dc8a29abb1a18e1f1155fda5fe0eb145a
Tomáš Havel <tomas.havel@tul.cz>
2025-11-06 12:59:13 +0100

    #8 Generator - vychozi projekt

---
+++ IS-Programy/program005-generator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+string again = "a";
+while (again == "a")
+{
+
+
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("****** Generator pseudonahodnych cisel ******");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*************** Tomáš Havel *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("**************** 6.11.2025 *****************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine("*********************************************");
+    Console.WriteLine();
+
+
+
+
+
+
+
+        Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
